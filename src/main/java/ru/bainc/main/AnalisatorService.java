e8ba9ae08b74647d444d061313df303a8050e985
Babushkina Olga <ba80@mail.ru>
2022-05-15 12:53:41 +0300

    Обновила

--- src/main/java/ru/bainc/main/AnalisatorService.java
+++ src/main/java/ru/bainc/main/AnalisatorService.java
@@ -10,7 +10,8 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class AnalisatorService {
-    public static Map<String, Integer> analizator (Path path) throws IOException {
+
+    public Map<String, Integer> analizator (Path path) throws IOException {
         List<String> words = new ArrayList<>();
    //     words = Collections.singletonList(FileUtils.readFileToString(new File(path.toString()), StandardCharsets.UTF_8.toString()));
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader((path.toString())))) {
@@ -23,12 +24,10 @@ public class AnalisatorService {
         }
         return words.stream().parallel()
                 .flatMap(line -> Stream.of(line.split("\\s+"))) // разбиваем строку на слова по пробелу 1 и более
-                .map(word -> word.replaceAll("[^А-Яа-яЁё]+", "") // выкидываем небуквенные символы
-                        .toLowerCase()) //   делаем все буквы маленькие
+                .map(word->word.toLowerCase()) // приводим все слова к строчным буквам
+                .map(word -> word.replaceAll("[^а-яё]+", "")) // выкидываем небуквенные символы
                 .filter(word -> word.length() > 2) // оставляем слова длиной 3 и больше букв
-                .collect(Collectors.toMap(word-> word,value->1,(numberRepet1,numberRepet2)->Integer.sum(numberRepet1,numberRepet2)));
+                .collect(Collectors.toMap(word-> word,value->1, (a, b) -> Integer.sum(a, b)));
     }
-
-
 }
 
--- src/main/java/ru/bainc/main/Main.java
+++ src/main/java/ru/bainc/main/Main.java
@@ -13,21 +13,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        AnalisatorService analisatorService = new AnalisatorService();
         long startTime = System.currentTimeMillis();
-        Map<String, Integer> mapResult;
-        mapResult = AnalisatorService.analizator((Paths.get(args[0])));
+        Map<String, Integer> mapResult = analisatorService.analizator(Paths.get(args[0]));
         System.out.println("\nвремя  выполнения программы = "
                 + (System.currentTimeMillis() - startTime) / 1000 + " seconds ");
-        System.out.println("Введите слово");
+        System.out.println("Введите слово или наберите /end для выхода из программы");
         Scanner scanner = new Scanner(System.in);
         String word;
+
         do {
             word = scanner.nextLine();
-            mapResult.containsKey(word);
-            System.out.println("Это слово встречается " + mapResult.get(word));
+            if (mapResult.containsKey(word)) {
+                System.out.println("Это слово встречается " + mapResult.get(word));
+            }
+            if (!mapResult.containsKey(word) && !word.equals("/end")) {
+                System.out.println("Такого слова нет");
+            }
         }
         while (!word.equals("/end"));
         System.out.println("Завершение работы программы");
+
     }
 }
 
